8a5fd69a967bf1abee49639a3feb1bae0a46808b
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-01-07 20:50:40 -0700

    add shared specs for widget parental relationships

--- spec/widget_spec.rb
+++ spec/widget_spec.rb
@@ -11,45 +11,51 @@ describe HTTParrot::Widget do
     specify{ subject.should respond_to(:parent!) }
   end
 
+  shared_examples "a parental relationship" do |meth|
+
+    it "adds new values" do 
+      current = HTTParrot::Widget.new(:first => 1, :second => 2)
+      current.method(meth).call({:first => 2, :second => 1, :third => 3})
+      current.third.should eq(3)
+    end
+
+    it "raises error when not a symbol or respond_to? to_hash" do 
+      expect{ subject.method(meth).call([]) }.to raise_error(/symbol/) 
+    end
+
+    it "raises error when parent factory does not exist" do 
+      expect{ subject.method(meth).call(:something) }.to raise_error(/unknown factory/i)
+    end
+
+    it "builds corresponding factory when symbol" do 
+      HTTParrot::ResponseFactory.define(:response) { |r| r.value = "Test" }
+      current = HTTParrot::Widget.new
+      current.method(meth).call(:response)
+      current.value.should eq("Test")
+    end
+
+    it "adds values from multiple parents" do 
+      current = HTTParrot::Widget.new
+      current.method(meth).call(:first => 1)
+      current.method(meth).call(:second => 2)
+      current.method(meth).call(:third => 3)
+      current.first.should eq(1)
+      current.second.should eq(2)
+      current.third.should eq(3)
+    end
+
+  end
+
   context "#parent" do
     before(:each) { HTTParrot::ResponseFactory.clear! }
+
+    it_behaves_like "a parental relationship", :parent
 
     it "does not overwrite existing values" do
       current = HTTParrot::Widget.new(:first => 1, :second => 2)
       current.parent({:first => 2, :second => 1})
       current.first.should eq(1)
       current.second.should eq(2)
-    end
-
-    it "adds new values" do 
-      current = HTTParrot::Widget.new(:first => 1, :second => 2)
-      current.parent({:first => 2, :second => 1, :third => 3})
-      current.third.should eq(3)
-    end
-
-    it "raises error when not a symbol or respond_to? to_hash" do 
-      expect{ subject.parent([]) }.to raise_error(/symbol/) 
-    end
-
-    it "raises error when parent factory does not exist" do 
-      expect{ subject.parent(:something) }.to raise_error(/unknown factory/i)
-    end
-
-    it "builds corresponding factory when symbol" do 
-      HTTParrot::ResponseFactory.define(:response) { |r| r.value = "Test" }
-      current = HTTParrot::Widget.new
-      current.parent(:response)
-      current.value.should eq("Test")
-    end
-
-    it "adds values from multiple parents" do 
-      current = HTTParrot::Widget.new
-      current.parent(:first => 1)
-      current.parent(:second => 2)
-      current.parent(:third => 3)
-      current.first.should eq(1)
-      current.second.should eq(2)
-      current.third.should eq(3)
     end
 
     it "respects parent call order for adding values" do 
@@ -66,6 +72,9 @@ describe HTTParrot::Widget do
   end
 
   context "#parent!" do 
+    before(:each) { HTTParrot::ResponseFactory.clear! }
+
+    it_behaves_like "a parental relationship", :parent!
 
   end
 
